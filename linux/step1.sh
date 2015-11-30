b94d9a48c138380fd30952c21ff67fc792e3aa8a
James Bailey <jbailey@codisms.com>
2015-11-29 22:02:57 -0800

    fix

--- linux/step1.sh
+++ linux/step1.sh
@@ -33,10 +33,10 @@ updateSystem() {
 updateFileSystem() {
 	if grep -q "^/dev/vdb1 /data" "/etc/mtab"; then
 		[ ! -f /etc/fstab.orig ] && cp /etc/fstab /etc/fstab.orig
-		cp -R /root/* /data/
-		cp .* /data/
-		cp -R /root/.ssh /data/
-		cp -R /root/.setup /data/
+		cp -R /root/* /data/ || true
+		cp .* /data/ || true
+		cp -R /root/.ssh /data/ || true
+		cp -R /root/.setup /data/ || true
 		sed 's|/data|/root|' /etc/fstab.orig > /etc/fstab
 	fi
 }
