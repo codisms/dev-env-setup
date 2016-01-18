f30b06eafd938ba0f1b53dbceddaa8dd8aa781f3
James Bailey <jbailey@codisms.com>
2016-01-18 07:48:28 +0000

    fail

--- linux/step2.sh
+++ linux/step2.sh
@@ -135,7 +135,7 @@ installRuby() {
 	#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 	curl -sSLO https://rvm.io/mpapis.asc && gpg --import mpapis.asc
 	[ -f mpapis.asc ] && rm -f mpapis.asc
-	curl -sSL https://get.rvm.io | bash -s --rails #> /dev/null
+	curl -sSL https://get.rvm.io | bash -s stable --rails > /dev/null
 
 	source /usr/local/rvm/scripts/rvm
 # 	source /etc/profile
