8bf2b431184a5f0c2edcf5fa40231ae5ee914c7d
James Bailey <jbailey@centos-linux.shared>
2017-04-04 13:02:45 -0700

    How to work with a sudo system and not running as root

--- linux/step1.sh
+++ linux/step1.sh
@@ -6,16 +6,17 @@ cd ~
 . ~/.setup/linux/functions
 
 setHostName() {
+	echo SUDO=$SUDO
 	echo Setting host name to "$1"...
-	[ ! -f  /etc/sysconfig/network.orig ] && cp -f /etc/sysconfig/network /etc/sysconfig/network.orig
-	sed "s|HOSTNAME=.\+\$|HOSTNAME=$1|" /etc/sysconfig/network.orig > /etc/sysconfig/network
+	[ ! -f  /etc/sysconfig/network.orig ] && $SUDO cp -f /etc/sysconfig/network /etc/sysconfig/network.orig
+	$SUDO sed "s|HOSTNAME=.\+\$|HOSTNAME=$1|" /etc/sysconfig/network.orig > /etc/sysconfig/network
 
 	if [ -f /var/lib/dhclient/dhclient-eth0.leases ]; then
-		[ ! -f /var/lib/dhclient/dhclient-eth0.leases.orig ] && cp /var/lib/dhclient/dhclient-eth0.leases /var/lib/dhclient/dhclient-eth0.leases.orig
-		sed 's|option host-name "[a-f0-9-]\+"|option host-name "'$1'"|' /var/lib/dhclient/dhclient-eth0.leases.orig > /var/lib/dhclient/dhclient-eth0.leases
+		[ ! -f /var/lib/dhclient/dhclient-eth0.leases.orig ] && $SUDO cp /var/lib/dhclient/dhclient-eth0.leases /var/lib/dhclient/dhclient-eth0.leases.orig
+		$SUDO sed 's|option host-name "[a-f0-9-]\+"|option host-name "'$1'"|' /var/lib/dhclient/dhclient-eth0.leases.orig > /var/lib/dhclient/dhclient-eth0.leases
 	fi
 
-	echo 127.0.0.1 $1>> /etc/hosts
+	$SUDO echo 127.0.0.1 $1>> /etc/hosts
 }
 
 #-----------------------------------------------------------------------------------------------------------
@@ -23,22 +24,22 @@ setHostName() {
 
 updateSystem() {
 	echo Configuration EPEL repository...
-	rpm -Uvh --quiet http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-	rpm -Uvh --quiet http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-	#yum install -y https://centos6.iuscommunity.org/ius-release.rpm
+	$SUDO rpm -Uvh --quiet http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+	$SUDO rpm -Uvh --quiet http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+	#$SUDO yum install -y https://centos6.iuscommunity.org/ius-release.rpm
 
 	echo Updating system...
-	yum -y update
+	$SUDO yum -y update
 }
 
 updateFileSystem() {
 	if grep -q "^/dev/vdb1 /data" "/etc/mtab"; then
-		[ ! -f /etc/fstab.orig ] && cp /etc/fstab /etc/fstab.orig
-		cp -R /root/* /data/ || true
-		cp .* /data/ || true
-		cp -R /root/.ssh /data/ || true
-		cp -R /root/.setup /data/ || true
-		sed 's|/data|/root|' /etc/fstab.orig > /etc/fstab
+		[ ! -f /etc/fstab.orig ] && $SUDO cp /etc/fstab /etc/fstab.orig
+		$SUDO cp -R /root/* /data/ || true
+		$SUDO cp .* /data/ || true
+		$SUDO cp -R /root/.ssh /data/ || true
+		$SUDO cp -R /root/.setup /data/ || true
+		$SUDO sed 's|/data|/root|' /etc/fstab.orig > /etc/fstab
 	fi
 }
 
