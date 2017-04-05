467c815efb1ca4ecc4c331b83fc8f54b0ada85c7
James Bailey <jbailey@codisms.com>
2017-04-05 17:26:11 -0600

    We're losing sudo somewhere

--- linux/step3.sh
+++ linux/step3.sh
@@ -5,37 +5,52 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 . ./functions
 
+echo SUDO1=`which sudo`
+
 #-----------------------------------------------------------------------------------------------------------
 # Installations
 
 postInstall() {
+echo SUDO2=`which sudo`
 	chmod 755 ${MY_HOME}
 	[ -d ${MY_HOME}/web ] && chown -R apache:apache ${MY_HOME}/web
 
+echo SUDO3=`which sudo`
 	startServices
+echo SUDO4=`which sudo`
 }
 
 installPackages() {
+echo SUDO5=`which sudo`
 	installPostgres
+echo SUDO6=`which sudo`
 	installVim
+echo SUDO7=`which sudo`
 	installTmux
+echo SUDO8=`which sudo`
 
 	postInstall
+echo SUDO9=`which sudo`
 }
 
 installPostgres() {
 	printSubHeader "Installing postgresql..."
 
+echo SUDO10=`which sudo`
 	yum install -y -q https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
+echo SUDO11=`which sudo`
 	yum install -y -q postgresql96-odbc postgresql96-devel postgresql96 postgresql96-contrib postgresql96-server
+echo SUDO12=`which sudo`
 	/usr/pgsql-9.6/bin/postgresql96-setup initdb
 	systemctl start postgresql-9.6.service
 	systemctl enable postgresql-9.6.service
+echo SUDO13=`which sudo`
 
 	ln -s ${MY_HOME}/.codisms/psqlrc ${MY_HOME}/.psqlrc
 }
 
 installVim() {
+echo SUDO14=`which sudo`
 	printSubHeader "Installing vim..."
 
  	cd ${MY_HOME}
@@ -60,14 +75,18 @@ installVim() {
  	echo Removing existing version of vi/vim...
  	yum -y -q remove vim-common vim-enhanced vim-minimal
 
+echo SUDO15=`which sudo`
  	printSubHeader "Setting vim as default..."
  	update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
  	update-alternatives --set editor /usr/bin/vim
  	update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
  	update-alternatives --set vi /usr/bin/vim
 
+echo SUDO16=`which sudo`
 	configureVim
+echo SUDO17=`which sudo`
 	installVimExtensions_YCM
+echo SUDO18=`which sudo`
 
 	cd ${MY_HOME}
 }
@@ -98,14 +117,17 @@ installVimExtensions_YCM() {
 }
 
 installTmux() {
+echo SUDO19=`which sudo`
 	echo Installing libevent 2.x...
 	yum install -y libevent-2* libevent-devel-2*
+echo SUDO20=`which sudo`
 
 	printSubHeader "Installing tmux..."
 
 	cd ${MY_HOME}
 	echo Cloning tmux...
 	git clone --depth=1 https://github.com/tmux/tmux.git
+echo SUDO21=`which sudo`
 
 	echo Compiling tmux...
 	cd tmux
@@ -113,26 +135,29 @@ installTmux() {
 	#./configure --prefix=/usr/local #--quiet > /dev/null
 	./configure --quiet > /dev/null
 	make --quiet > /dev/null
+echo SUDO21=`which sudo`
 
 	echo Installing tmux...
 	make install --quiet > /dev/null
 	cd ..
 	rm -rf tmux
+echo SUDO22=`which sudo`
 
-  	PATH=$PATH:`find /opt/rh/rh-ruby*/root/usr/bin | head -n 1`
-
-# 	gem --update system
+ 	#gem --update system
 	gem install tmuxinator > /dev/null
+echo SUDO23=`which sudo`
 
 	printSubHeader "Downloading tmux configuration..."
 	cd ${MY_HOME}
 	git clone https://github.com/codisms/tmux-config.git .tmux
+echo SUDO24=`which sudo`
 
 	echo "Downloading submodules..."
 	cd .tmux
 	#git submodule init
 	git submodule update --init --recursive
 	cd ..
+echo SUDO25=`which sudo`
 
 	printSubHeader "Configuring tmux..."
 	ln -s .tmux/tmux.conf .tmux.conf
@@ -141,11 +166,13 @@ installTmux() {
 startServices() {
 	printSubHeader "Starting services..."
 
+echo SUDO26=`which sudo`
 	#startMySql
 
 	echo Disabling firewalld...
-	systemctl drop firewalld.service
+	systemctl stop firewalld.service
 	systemctl disable firewalld.service
+echo SUDO27=`which sudo`
 }
 
 startMySql() {
@@ -170,5 +197,6 @@ scheduleForNextRun "${MY_HOME}/.setup/linux/step4.sh"
 printHeader "Finished step 3.  Rebooting..."
 # read -p 'Press [Enter] to continue...'
 
+echo SUDO28=`which sudo`
 reboot
 
