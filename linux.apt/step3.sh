25687f4b8a805b9bd8db8ef737b16ef24511ad5a
James Bailey <jbailey@codisms.com>
2018-04-20 21:34:33 -0700

    Handle if ufw doesn't exist

--- linux.apt/step3.sh
+++ linux.apt/step3.sh
@@ -126,8 +126,10 @@ startServices() {
 
 	#startMySql
 
-	echo Disabling firewalld...
-	$SUDO ufw disable
+	if hash ufw 2>/dev/null; then
+		echo Disabling firewalld...
+		$SUDO ufw disable
+	fi
 }
 
 startMySql() {
