126bc95e2d76468cfa7627acb5459c36e3358c39
James Bailey <jbailey@codisms.com>
2018-04-20 21:38:51 -0700

    Move installing fonts to step 3

--- linux.apt/step3.sh
+++ linux.apt/step3.sh
@@ -8,6 +8,19 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 #-----------------------------------------------------------------------------------------------------------
 # Installations
 
+installFonts() {
+	retry pip install --user powerline-status
+
+	## https://gist.github.com/renshuki/3cf3de6e7f00fa7e744a
+	#mkdir -p ~/.fonts
+	#mkdir -p ~/.config/fontconfig/conf.d
+
+	#curl https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -o ~/.fonts/PowerlineSymbols.otf -L
+	#curl https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -o ~/.config/fontconfig/conf.d/10-powerline-symbols.conf -L
+
+	#fc-cache -vf ~/.fonts/
+}
+
 postInstall() {
 	$SUDO chmod 755 ${MY_HOME}
 	[ -d ${MY_HOME}/web ] && chown -R apache:apache ${MY_HOME}/web
@@ -142,6 +155,9 @@ startMySql() {
 ############################################################################################################
 # BEGIN
 ############################################################################################################
+
+printHeader "Installing fonts..."
+installFonts
 
 printHeader "Installing packages..."
 installPackages
