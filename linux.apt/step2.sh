16c230f59435c9e654279dfdde282cc44ff63a06
James Bailey <jbailey@codisms.com>
2018-05-18 23:26:11 -0700

    Fixing

--- linux.apt/step2.sh
+++ linux.apt/step2.sh
@@ -127,12 +127,12 @@ installNode() {
 
 installRuby() {
 	apt_get_install software-properties-common
-	apt_add_repository ppa:rael-gc/rvm
-	apt_get_update
-	apt_get_install rvm
+	#apt_add_repository ppa:rael-gc/rvm
+	#apt_get_update
+	#apt_get_install rvm
 
-	echo "** ext"
-	exit
+	gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+	curl -sSL https://get.rvm.io | bash -s stable
 
 	source /etc/profile.d/rvm.sh
 	rvm install ruby-2.3
