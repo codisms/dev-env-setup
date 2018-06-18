f1dca2b886eacd778869d71e7cd5ce20912d24a8
James Bailey <jbailey@codisms.com>
2018-06-18 12:41:34 -0700

    Another apache mod

--- linux.apt/step3.sh
+++ linux.apt/step3.sh
@@ -44,7 +44,7 @@ postInstall() {
 	[ -d ${MY_HOME}/web ] && chown -R apache:apache ${MY_HOME}/web
 
 	printSubHeader "Configuring apache modules"
-	$SUDO a2enmod proxy proxy_http proxy_wstunnel rewrite auth_basic proxy_balancer proxy_html proxy_connect ssl xml2enc
+	$SUDO a2enmod proxy proxy_http proxy_wstunnel rewrite auth_basic proxy_balancer proxy_html proxy_connect ssl xml2enc substitute
 
 	startServices
 }
