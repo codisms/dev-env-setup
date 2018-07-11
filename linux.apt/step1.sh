cb4487dac952721458f88d8788f7bb9ab9cb7832
James Bailey <jbailey@codisms.com>
2018-07-10 18:23:03 -0700

    Locale fix?

--- linux.apt/step1.sh
+++ linux.apt/step1.sh
@@ -68,15 +68,19 @@ installAptFast() {
 	reloadEnvironment
 }
 
-updateSystem() {
-	printHeader "Updating system..." "system"
-
+fixLocale() {
 	if [ "$LC_CTYPE" == "" ] || [ "$LC_ALL" == "" ]; then
+		printHeader "Fixing locales...", "locale"
+
 		#export LC_ALL="en_US.UTF-8"
 		#export LC_CTYPE="en_US.UTF-8"
 		#retry_long $SUDO dpkg-reconfigure locales
 		apt_get_install locales language-pack-en
 	fi
+}
+
+updateSystem() {
+	printHeader "Updating system..." "system"
 
 	apt_get_update
 
@@ -116,6 +120,7 @@ if [ "$1" != "" ]; then
 	setHostName $1
 fi
 
+fixLocale
 checkForSwap
 installAptFast
 updateSystem
