299f7dcc8bd52112c08df1ba0169cf2549394a0e
James Bailey <jbailey@codisms.com>
2018-10-11 16:42:18 -0700

    It wasn't autoremove causing the issue; it's certbot

--- linux.apt/final-cleanup.sh
+++ linux.apt/final-cleanup.sh
@@ -1,3 +1,2 @@
 printHeader "Removing unused packages...", "autoremove"
-$SUDO service apache2 stop
 apt_get_autoremove
--- linux.apt/install-tools.sh
+++ linux.apt/install-tools.sh
@@ -3,7 +3,8 @@ installCertbot() {
 	printSubHeader "Installing certbot"
 	apt_add_repository ppa:certbot/certbot
 	apt_get_update
-	apt_get_install python-certbot-apache python-certbot-nginx
+	apt_get_install python-certbot-apache
+	#apt_get_install python-certbot-nginx
 }
 
 installAwsCli() {
