e81d134ccba3a096a9507273fb57137885ac588a
James Bailey <jbailey@codisms.com>
2018-10-12 21:15:16 -0700

    Quieter apt-fast

--- scripts/install-apt-fast.sh
+++ scripts/install-apt-fast.sh
@@ -5,6 +5,6 @@ apt_get_update
 apt_get_install apt-fast
 #mkdir -p ${MY_HOME}/.config/aria2/
 #echo "log-level=warn" >> ~/.config/aria2/input.conf
-sudo sed -i 's|--no-conf |--no-conf --console-log-level=warn|' /etc/apt-fast.conf
+sudo sed -i 's|--no-conf |--no-conf --console-log-level=warn |' /etc/apt-fast.conf
 
 reloadEnvironment
