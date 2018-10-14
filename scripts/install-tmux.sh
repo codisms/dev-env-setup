bf04bda09d55b883efe2257e95cf2acc6219e5e7
James Bailey <jbailey@codisms.com>
2018-10-14 08:40:46 -0700

    Fix tmux and vim scripts

--- scripts/install-tmux.sh
+++ scripts/install-tmux.sh
@@ -2,4 +2,4 @@ printSubHeader "Installing libevent 2.x..."
 apt_get_install libevent-2* libevent-dev
 
 printSubHeader "Installing tmux..."
-curl -sSL -H 'Cache-Control: no-cache' https://github.com/codisms/tmux-config/raw/master/install.sh | bash -s --build --version=2.6
+curl -sSL -H 'Cache-Control: no-cache' https://github.com/codisms/tmux-config/raw/master/install.sh | bash -s -- --build --version=2.6
