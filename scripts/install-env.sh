2a30a7e74c1b82d3a0689b097ba5859a500382a8
James Bailey <codisms@users.noreply.github.com>
2018-10-17 09:27:26 -0700

    Update install-env.sh

--- scripts/install-env.sh
+++ scripts/install-env.sh
@@ -1,4 +1,4 @@
 printSubHeader "Installing env-config..."
-curl -sSL -H 'Cache-Control: no-cache' https://github.com/codisms/env-config/raw/master/install.sh | bash -s
+curl -sSL https://github.com/codisms/env-config/raw/master/install.sh | bash -s
 
 reloadEnvironment
