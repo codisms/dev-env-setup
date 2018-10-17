54de22e2c93ea1ae332d0f5374569e4ff32ff96e
James Bailey <codisms@users.noreply.github.com>
2018-10-17 09:27:44 -0700

    Update install-vim.sh

--- scripts/install-vim.sh
+++ scripts/install-vim.sh
@@ -1,3 +1,3 @@
 printSubHeader "Installing vim..."
-PATH="${PATH}" curl -sSL -H 'Cache-Control: no-cache' https://github.com/codisms/vim-config/raw/master/install.sh | bash -s -- --build
+PATH="${PATH}" curl -sSL https://github.com/codisms/vim-config/raw/master/install.sh | bash -s -- --build
 
