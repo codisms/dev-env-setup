f6c842724d78b7d9cfcd9acb7e059576abf9a901
James Bailey <jbailey@codisms.com>
2018-12-26 21:10:49 -0800

    Debug statements for chrome

--- scripts/install-chrome.sh
+++ scripts/install-chrome.sh
@@ -1,11 +1,6 @@
 
 cd /tmp
-echo "*1"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-echo "*2"
 $SUDO apt-get -fy install software-properties-common fonts-liberation libappindicator3-1 libatk-bridge2.0-0 libgtk-3-0
-echo "*3"
 $SUDO apt-get -fy install
-echo "*4"
 $SUDO dpkg -i google-chrome*.deb
-echo "*5"
