0e84b1d38606cdd298433e452add552ccd4fc896
James Bailey <jbailey@codisms.com>
2018-12-26 18:44:45 -0800

    Add flag for dotfiles?

--- scripts/install-languages.sh
+++ scripts/install-languages.sh
@@ -60,12 +60,12 @@ installRuby() {
 
 	printSubHeader "Downloading GPG keys..."
 	retry gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-	curl -o- -sSL https://get.rvm.io | bash -s stable --auto-dotfiles
+	curl -o- -sSL https://get.rvm.io | bash -s stable
 	reloadEnvironment
 	#source ${HOME}/.rvm/scripts/rvm
 
 	printSubHeader "Downloading and installying Ruby..."
-	retry rvm install ruby-2.3
+	retry rvm install ruby-2.3 --auto-dotfiles
 	#source /etc/profile.d/rvm.sh
 	#source ${HOME}/.rvm/scripts/rvm
 	reloadEnvironment
