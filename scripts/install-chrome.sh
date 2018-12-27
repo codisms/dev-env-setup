9c03ae12f5bf0183f9ae0c5f1281880d8dd2d87e
James Bailey <jbailey@codisms.com>
2018-12-26 21:35:02 -0800

    Non-interactive?

--- scripts/install-chrome.sh
+++ scripts/install-chrome.sh
@@ -1,6 +1,6 @@
 
 cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-$SUDO apt-get -fy install software-properties-common fonts-liberation libappindicator3-1 libatk-bridge2.0-0 libgtk-3-0
-$SUDO apt-get -fy install
-$SUDO dpkg -i google-chrome*.deb
+$SUDO DEBIAN_FRONTEND=noninteractive apt-get -fy install software-properties-common fonts-liberation libappindicator3-1 libatk-bridge2.0-0 libgtk-3-0
+$SUDO DEBIAN_FRONTEND=noninteractive apt-get -fy install
+$SUDO DEBIAN_FRONTEND=noninteractive dpkg -i google-chrome*.deb
