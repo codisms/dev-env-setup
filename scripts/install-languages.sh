4336f00d1814b3250bf2447d6b3860e6460cd9e4
James Bailey <jbailey@codisms.com>
2018-12-26 21:02:36 -0800

    Source rvm

--- scripts/install-languages.sh
+++ scripts/install-languages.sh
@@ -67,7 +67,7 @@ installRuby() {
 	retry gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 	curl -o- -sSL https://get.rvm.io | bash -s stable
 	reloadEnvironment
-	#source ${HOME}/.rvm/scripts/rvm
+	source ${HOME}/.rvm/scripts/rvm
 
 	printSubHeader "Downloading and installying Ruby..."
 	retry rvm install ruby-2.3
