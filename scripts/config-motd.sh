90c9277c5066c90944af1396e66e1bbe0b8228e8
James Bailey <jbailey@codisms.com>
2019-02-12 13:04:50 -0800

    Standardize

--- scripts/config-motd.sh
+++ scripts/config-motd.sh
@@ -1,4 +1,4 @@
 echo "Setting motd..."
 [ -f /etc/motd ] && $SUDO mv /etc/motd /etc/motd.orig
-echo $(hostname) | figlet -d $HOME/.setup/ -f "ANSI Shadow" > /tmp/motd
+echo $(hostname) | figlet -d ${HOME}/.setup/ -f "ANSI Shadow" > /tmp/motd
 $SUDO mv /tmp/motd /etc/motd
