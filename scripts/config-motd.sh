f0e0d34de93bd9e781b3be27eced6ad7d5c6d464
James Bailey <jbailey@codisms.com>
2019-02-12 13:02:09 -0800

    Set up motd here

--- scripts/config-motd.sh
+++ scripts/config-motd.sh
@@ -1,2 +1,4 @@
 echo "Setting motd..."
-${HOME}/.dotfiles/config-motd.sh
+[ -f /etc/motd ] && $SUDO mv /etc/motd /etc/motd.orig
+echo $(hostname) | figlet -d $HOME/.setup/ -f "ANSI Shadow" > /tmp/motd
+$SUDO mv /tmp/motd /etc/motd
