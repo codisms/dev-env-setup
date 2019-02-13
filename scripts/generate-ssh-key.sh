b5932a40c3ca5fb2b7a74a4097d6b77fac7bab44
James Bailey <jbailey@codisms.com>
2019-02-12 23:14:02 -0800

    Check for existing SSH key before keygen

--- scripts/generate-ssh-key.sh
+++ scripts/generate-ssh-key.sh
@@ -1,7 +1,9 @@
-#echo
-#echo -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-#echo Generating SSH key...
-##read -p 'Press [Enter] to continue...'
+if [ ! -f ${HOME}/.ssh/id_rsa ]; then
+	#echo
+	#echo -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	#echo Generating SSH key...
+	##read -p 'Press [Enter] to continue...'
 
-#echo
-ssh-keygen -t rsa -N "" -C "$(hostname)" -f ${HOME}/.ssh/id_rsa
+	#echo
+	ssh-keygen -t rsa -N "" -C "$(hostname)" -f ${HOME}/.ssh/id_rsa
+fi
