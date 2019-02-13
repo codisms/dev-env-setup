401112783b298f40d3e1a827a315967fd60221f2
James Bailey <jbailey@codisms.com>
2019-02-13 14:24:55 -0800

    Debug install-env

--- scripts/install-env.sh
+++ scripts/install-env.sh
@@ -1,4 +1,7 @@
 printSubHeader "Installing env-config..."
+echo "USE_BASH = ${USE_BASH}"
+echo "bash = $(option_set bash)"
+
 if [ $USE_BASH -eq 1 ]; then
 	curl -sSL https://github.com/codisms/env-config/raw/master/install.sh | bash -s
 else
