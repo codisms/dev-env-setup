3584fe7beb212262ade3826d3dcc1cd663eb85c5
James Bailey <jbailey@codisms.com>
2019-02-12 22:14:27 -0800

    Fix docker install

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -2,9 +2,9 @@ apt_get_install apt-transport-https ca-certificates curl gnupg2 software-propert
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | $SUDO apt-key add -
 
-$SUDO add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+add_apt_repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 	$(lsb_release -cs) stable"
 
-$SUDO apt-get update
-$SUDO apt-get install docker-ce
+apt_get_update
+apt_get_install docker-ce
 
