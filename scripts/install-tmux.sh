db6738195585c090c9c61f067954fcc410c66dfa
James Bailey <jbailey@codisms.com>
2019-06-01 04:12:00 +0000

    Install latest version of tmux instead of fixed at 2.7

--- scripts/install-tmux.sh
+++ scripts/install-tmux.sh
@@ -2,4 +2,5 @@ printSubHeader "Installing libevent 2.x..."
 apt_get_install libevent-2* libevent-dev
 
 printSubHeader "Installing tmux..."
-curl -sSL https://github.com/codisms/tmux-config/raw/master/install.sh | bash -s -- --build --version=2.7
+#curl -sSL https://github.com/codisms/tmux-config/raw/master/install.sh | bash -s -- --build --version=2.9
+curl -sSL https://github.com/codisms/tmux-config/raw/master/install.sh | bash -s -- --build
