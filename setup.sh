740e0cbdb472a0e838e083c073da49440ef70f3e
James Bailey <jbailey@codisms.com>
2019-06-21 09:54:22 -0700

    Add ability to test from branches; trying to fix verbosity and wait_for_apt()

--- setup.sh
+++ setup.sh
@@ -17,15 +17,19 @@ PACKAGE_MANAGER=$(get_package_manager)
 SCRIPTS_FOLDER="${HOME}/.setup/scripts"
 SCRIPT_FILE="${SCRIPTS_FOLDER}/setup.${PACKAGE_MANAGER}.txt"
 NEW_HOST_NAME=$(option_value host)
+BRANCH=$(option_value branch)
 USE_BASH=$(option_set bash)
 #DEBUG=${option_set debug}
 DEBUG=1
 VERBOSE=${option_set verbose}
+echo "verbose = $VERBOSE"
 if [ $VERBOSE -eq 1 ]; then
 	set -x
 fi
 
 debug "DEBUG = ${DEBUG}"
+debug "VERBOSE = ${VERBOSE}"
+debug "BRANCH = ${BRANCH}"
 debug "USE_BASH = ${USE_BASH}"
 debug "HOME = ${HOME}"
 debug "USER = ${USER}"
