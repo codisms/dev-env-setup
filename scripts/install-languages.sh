dc15a900370698912af37172421eee803fc0aab4
James Bailey <codisms@users.noreply.github.com>
2021-06-22 13:41:27 -0700

    Update rvm install

--- scripts/install-languages.sh
+++ scripts/install-languages.sh
@@ -72,7 +72,7 @@ installRuby() {
 	fi
 
 	printSubHeader "Downloading GPG keys..."
-	retry gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+	retry gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 	curl -o- -sSL https://get.rvm.io | bash -s stable
 	reloadEnvironment
 	source ${HOME}/.rvm/scripts/rvm
