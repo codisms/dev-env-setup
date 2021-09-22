0cf9901f3e62f707fa241c1bfed8d266729cae54
James Bailey <jbailey@codisms.com>
2021-09-22 10:04:45 -0700

    Fix function name from last commit.

--- scripts/fix-locale.sh
+++ scripts/fix-locale.sh
@@ -5,6 +5,6 @@ if [ "$LC_CTYPE" == "" ] || [ "$LC_ALL" == "" ]; then
 	#export LC_CTYPE="en_US.UTF-8"
 	#retry_long $SUDO dpkg-reconfigure locales
 	apt_get_install locales
-	apt_update
+	apt_get_update
 	apt_get_install language-pack-en
 fi
