89cf6e72aee7725ab3962d1f922e37fdd270a47a
James Bailey <jbailey@codisms.com>
2023-05-02 14:19:02 -0700

    Add Google Cloud CLI

--- scripts/install-gcloud.sh
+++ scripts/install-gcloud.sh
@@ -1,4 +1,4 @@
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | $SUDO apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | $SUDO tee -a /etc/apt/sources.list.d/kubernetes.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | $SUDO apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | $SUDO tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 apt_get_update
-apt_get_install kubectl
+apt_get_install google-cloud-cli
